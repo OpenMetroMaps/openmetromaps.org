29a36e6fc1d3347eb59a667814d5c41043717a06
Sebastian Kürten <sebastian@topobyte.de>
2017-06-12 11:37:33 +0200

    Add link to twitter and some introductory text

--- project/dynamic/src/main/java/org/openmetromaps/pages/IndexGenerator.java
+++ project/dynamic/src/main/java/org/openmetromaps/pages/IndexGenerator.java
@@ -36,12 +36,27 @@ public class IndexGenerator extends BaseGenerator
 		slogan.attr("style",
 				"display:block; font-weight: normal; text-align: center");
 
-		content.ac(HTML.h1("Coming soon..."));
+		content.ac(HTML.h1("Get in touch"));
 
 		Element stuff = content.ac(HTML.span());
 		stuff.appendText("Our code is going to be hosted on GitHub: ");
-		A link = stuff.ac(HTML.a("https://github.com/OpenMetroMaps"));
-		link.appendText("https://github.com/OpenMetroMaps");
+		A linkGithub = stuff.ac(HTML.a("https://github.com/OpenMetroMaps"));
+		linkGithub.appendText("https://github.com/OpenMetroMaps");
+
+		stuff.appendChild(HTML.br());
+
+		stuff.appendText("Follow us on Twitter: ");
+		A linkTwitter = stuff.ac(HTML.a("https://twitter.com/openmetromaps"));
+		linkTwitter.appendText("@openmetromaps");
+
+		content.ac(HTML.h1("What's this?"));
+
+		content.appendText("Schmeatic maps for public transportation networks are essential tools for orientation and navigation in cities.");
+		content.appendText(" Unfortunately, these maps are usually proprietary resources that pose strong limits on what you are allowed to do with them.");
+		content.appendText(" The owners of the material often charge a fee for displaying the content on websites, apps or printed material and they don't allow you to change anything on the map itself.");
+		content.appendText(" This alone prevents creative use of these maps.");
+		content.appendText(" Furthermore, you only get a PDF document – this makes interactive content hard to create.");
+		content.appendText(" We're here to change that by providing tools and file formats for creating free schematic metro maps.");
 
 		footer();
 	}
