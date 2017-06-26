95888072dc29216d6719bbe537645491947e609b
Sebastian Kürten <sebastian@topobyte.de>
2017-06-26 12:43:57 +0200

    Deploy to new server

--- project/deploy.to.production.locally.sh
+++ project/deploy.to.production.locally.sh
@@ -5,4 +5,4 @@ set -e
 ./gradlew clean war
 
 su -c 'cp dynamic/build/libs/openmetromaps-dynamic-0.0.1-production.war \
-/opt/tomcat/apache-tomcat-8.0.33-other/webapps/openmetromaps/ROOT.war'
+/opt/tomcat/tomcat-main/rootapps/openmetromaps/ROOT.war'
--- project/deploy.to.production.sh
+++ project/deploy.to.production.sh
@@ -5,7 +5,7 @@ set -e
 ./gradlew clean war
 
 scp dynamic/build/libs/openmetromaps-dynamic-0.0.1-production.war \
-root@topobyte.de:/tmp/openmetromaps.war
+root@web1.topobyte.de:/tmp/openmetromaps.war
 
-ssh root@topobyte.de mv /tmp/openmetromaps.war \
-/opt/tomcat/apache-tomcat-8.0.33-other/webapps/openmetromaps/ROOT.war
+ssh root@web1.topobyte.de mv /tmp/openmetromaps.war \
+/opt/tomcat/tomcat-main/rootapps/openmetromaps/ROOT.war
