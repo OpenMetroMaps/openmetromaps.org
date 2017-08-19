417028b29b767228eb8d20e0b79061be8ae50946
Sebastian Kürten <sebastian@topobyte.de>
2017-08-19 19:18:34 +0200

    Use rsync for deployment

--- project/deploy.to.production.sh
+++ project/deploy.to.production.sh
@@ -4,8 +4,5 @@ set -e
 
 ./gradlew clean war
 
-scp dynamic/build/libs/openmetromaps-dynamic-0.0.1-production.war \
-root@web1.topobyte.de:/tmp/openmetromaps.war
-
-ssh root@web1.topobyte.de mv /tmp/openmetromaps.war \
-/opt/tomcat/tomcat-main/rootapps/openmetromaps/ROOT.war
+rsync -av dynamic/build/libs/openmetromaps-dynamic-0.0.1-production.war \
+root@web1.topobyte.de:/opt/tomcat/tomcat-main/rootapps/openmetromaps/ROOT.war
