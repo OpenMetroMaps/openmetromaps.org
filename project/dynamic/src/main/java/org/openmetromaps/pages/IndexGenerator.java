f4caa78a009501a5de59bd12a052619fcc418006
Sebastian Kürten <sebastian@topobyte.de>
2017-09-05 20:41:17 +0200

    Add funding section with BMBF logo

--- project/dynamic/src/main/java/org/openmetromaps/pages/IndexGenerator.java
+++ project/dynamic/src/main/java/org/openmetromaps/pages/IndexGenerator.java
@@ -6,6 +6,8 @@ import org.openmetromaps.BaseGenerator;
 
 import de.topobyte.jsoup.HTML;
 import de.topobyte.jsoup.components.A;
+import de.topobyte.jsoup.components.Div;
+import de.topobyte.jsoup.components.Img;
 import de.topobyte.jsoup.nodes.Element;
 import de.topobyte.pagegen.core.Context;
 import de.topobyte.webpaths.WebPath;
@@ -57,6 +59,29 @@ public class IndexGenerator extends BaseGenerator
 		content.appendText(" This alone prevents creative use of these maps.");
 		content.appendText(" Furthermore, you only get a PDF document – this makes interactive content hard to create.");
 		content.appendText(" We're here to change that by providing tools and file formats for creating free schematic metro maps.");
+
+		content.ac(HTML.h1("Funding"));
+
+		A linkPF = stuff.ac(HTML.a("https://prototypefund.de"));
+		linkPF.appendText("Prototype Fund");
+
+		A linkOKFN = stuff.ac(HTML.a("https://okfn.de"));
+		linkOKFN.appendText("Open Knowledge Foundation Germany");
+
+		A linkBMBF = stuff.ac(HTML.a("https://www.bmbf.de"));
+		linkBMBF.appendText("German Federal Ministry of Education and Resesarch");
+
+		content.appendText("This project is funded by the");
+		content.ap(linkOKFN);
+		content.appendText(" via the");
+		content.ap(linkPF);
+		content.appendText(", which is backed by the");
+		content.ap(linkBMBF);
+		content.appendText(".");
+
+		Div div = content.ac(HTML.div());
+		Img img = div.ac(HTML.img("images/BMBF_en.jpg"));
+		img.attr("style", "height:16em;width:auto");
 
 		footer();
 	}
