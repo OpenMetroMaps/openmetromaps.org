a402e4eed35395f3c5d960841a36df9d0ffb3c79
Sebastian Kürten <sebastian@topobyte.de>
2017-12-04 10:31:10 +0100

    Use shorter notation for links

--- project/dynamic/src/main/java/org/openmetromaps/pages/IndexGenerator.java
+++ project/dynamic/src/main/java/org/openmetromaps/pages/IndexGenerator.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import org.openmetromaps.BaseGenerator;
 
 import de.topobyte.jsoup.HTML;
-import de.topobyte.jsoup.components.A;
 import de.topobyte.jsoup.components.Div;
 import de.topobyte.jsoup.components.Img;
 import de.topobyte.jsoup.nodes.Element;
@@ -29,8 +28,7 @@ public class IndexGenerator extends BaseGenerator
 
 		Element headline = content.ac(HTML.span());
 		headline.appendText("Open Metro Maps");
-		headline.attr(
-				"style",
+		headline.attr("style",
 				"display:block; font-family: 'Sans', serif; font-weight: normal; font-size: 36pt; text-align: center");
 
 		Element slogan = content.ac(HTML.span());
@@ -42,52 +40,48 @@ public class IndexGenerator extends BaseGenerator
 
 		Element stuff = content.ac(HTML.span());
 		stuff.appendText("Our code is hosted on ");
-		A linkGithub = stuff.ac(HTML.a("https://github.com/OpenMetroMaps"));
-		linkGithub.appendText("GitHub");
+		stuff.ac(HTML.a("https://github.com/OpenMetroMaps", "GitHub"));
 		stuff.appendText(". ");
 
 		stuff.appendText("You can follow us on ");
-		A linkTwitter = stuff.ac(HTML.a("https://twitter.com/openmetromaps"));
-		linkTwitter.appendText("Twitter");
+		stuff.ac(HTML.a("https://twitter.com/openmetromaps", "Twitter"));
 
-		stuff.appendText(" or have a look at the contributions we submitted to the OpenStreetMap with ");
-		A linkOsmProfile = stuff.ac(HTML
-				.a("https://www.openstreetmap.org/user/OpenMetroMaps"));
-		linkOsmProfile.appendText("our OSM account");
+		stuff.appendText(
+				" or have a look at the contributions we submitted to the OpenStreetMap with ");
+		stuff.ac(HTML.a("https://www.openstreetmap.org/user/OpenMetroMaps",
+				"our OSM account"));
 		stuff.appendText(". ");
 
 		stuff.appendText("There's also a ");
-		A linkMailingList = stuff.ac(HTML
-				.a("https://www.freelists.org/list/openmetromaps"));
-		linkMailingList.appendText("Mailing List");
-		stuff.appendText(" that you can subscribe to if you want to participate in discussions around the project.");
+		stuff.ac(HTML.a("https://www.freelists.org/list/openmetromaps",
+				"Mailing List"));
+		stuff.appendText(
+				" that you can subscribe to if you want to participate in discussions around the project.");
 
 		content.ac(HTML.h1("What's this?"));
 
-		content.appendText("Schematic maps for public transportation networks are essential tools for orientation and navigation in cities.");
-		content.appendText(" Unfortunately, these maps are usually proprietary resources that pose strong limits on what you are allowed to do with them.");
-		content.appendText(" The owners of the material often charge a fee for displaying the content on websites, apps or printed material and they don't allow you to change anything on the map itself.");
+		content.appendText(
+				"Schematic maps for public transportation networks are essential tools for orientation and navigation in cities.");
+		content.appendText(
+				" Unfortunately, these maps are usually proprietary resources that pose strong limits on what you are allowed to do with them.");
+		content.appendText(
+				" The owners of the material often charge a fee for displaying the content on websites, apps or printed material and they don't allow you to change anything on the map itself.");
 		content.appendText(" This alone prevents creative use of these maps.");
-		content.appendText(" Furthermore, you only get a PDF document – this makes interactive content hard to create.");
-		content.appendText(" We're here to change that by providing tools and file formats for creating free schematic metro maps.");
+		content.appendText(
+				" Furthermore, you only get a PDF document – this makes interactive content hard to create.");
+		content.appendText(
+				" We're here to change that by providing tools and file formats for creating free schematic metro maps.");
 
 		content.ac(HTML.h1("Funding"));
 
-		A linkPF = stuff.ac(HTML.a("https://prototypefund.de"));
-		linkPF.appendText("Prototype Fund");
-
-		A linkOKFN = stuff.ac(HTML.a("https://okfn.de"));
-		linkOKFN.appendText("Open Knowledge Foundation Germany");
-
-		A linkBMBF = stuff.ac(HTML.a("https://www.bmbf.de"));
-		linkBMBF.appendText("German Federal Ministry of Education and Resesarch");
-
 		content.appendText("This project is funded by the");
-		content.ap(linkOKFN);
+		content.ac(
+				HTML.a("https://okfn.de", "Open Knowledge Foundation Germany"));
 		content.appendText(" via the");
-		content.ap(linkPF);
+		content.ac(HTML.a("https://prototypefund.de", "Prototype Fund"));
 		content.appendText(", which is backed by the");
-		content.ap(linkBMBF);
+		content.ac(HTML.a("https://www.bmbf.de",
+				"German Federal Ministry of Education and Resesarch"));
 		content.appendText(".");
 
 		Div div = content.ac(HTML.div());
