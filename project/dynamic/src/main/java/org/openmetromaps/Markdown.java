703ed7d34655c63edcb9ac102b4266f4f0ef8502
Sebastian Kürten <sebastian@topobyte.de>
2017-12-12 13:02:03 +0100

    Add support for wiki links

--- project/dynamic/src/main/java/org/openmetromaps/Markdown.java
+++ project/dynamic/src/main/java/org/openmetromaps/Markdown.java
@@ -6,6 +6,7 @@ import com.vladsch.flexmark.Extension;
 import com.vladsch.flexmark.ast.Document;
 import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
 import com.vladsch.flexmark.ext.tables.TablesExtension;
+import com.vladsch.flexmark.ext.wikilink.WikiLinkExtension;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.options.MutableDataHolder;
@@ -31,8 +32,10 @@ public class Markdown
 		};
 
 		Extension strikethrough = StrikethroughExtension.create();
+		Extension wikilink = WikiLinkExtension.create();
 
-		options.set(Parser.EXTENSIONS, Arrays.asList(tables, strikethrough));
+		options.set(Parser.EXTENSIONS,
+				Arrays.asList(tables, strikethrough, wikilink));
 
 		Parser parser = Parser.builder(options).build();
 		HtmlRenderer renderer = HtmlRenderer.builder(options).build();
