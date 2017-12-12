d8285cce03cec3d5477a8caab8832c250e76f553
Sebastian Kürten <sebastian@topobyte.de>
2017-12-12 13:43:07 +0100

    Convert most of index page to markdown

--- project/dynamic/src/main/java/org/openmetromaps/pages/IndexGenerator.java
+++ project/dynamic/src/main/java/org/openmetromaps/pages/IndexGenerator.java
@@ -3,7 +3,7 @@ package org.openmetromaps.pages;
 import java.io.IOException;
 
 import org.openmetromaps.BaseGenerator;
-import org.openmetromaps.PathHelper;
+import org.openmetromaps.Markdown;
 
 import de.topobyte.jsoup.HTML;
 import de.topobyte.jsoup.components.Div;
@@ -37,122 +37,11 @@ public class IndexGenerator extends BaseGenerator
 		slogan.attr("style",
 				"display:block; font-weight: normal; text-align: center");
 
-		mission();
-
-		status();
-
-		license();
-
-		documentation();
+		Markdown.renderResource(content, "markdown/index.md");
 
 		funding();
 
 		footer();
-	}
-
-	private void mission()
-	{
-		content.ac(HTML.h1("Our mission"));
-
-		content.appendText(
-				"Schematic maps for public transportation networks are essential tools for orientation and navigation in cities.");
-		content.appendText(
-				" Unfortunately, these maps are usually proprietary resources that pose strong limits on what you are allowed to do with them.");
-		content.appendText(
-				" The owners of the material often charge a fee for displaying the content on websites, apps or printed material and they don't allow you to change anything on the map itself.");
-		content.appendText(" This alone prevents creative use of these maps.");
-		content.appendText(
-				" Furthermore, you only get a PDF document – this makes interactive content hard to create.");
-		content.appendText(
-				" We're here to change that by providing open source tools and file formats for creating free schematic metro maps.");
-
-		content.ac(HTML.a(getLink(PathHelper.mission()), "Read more"));
-		content.appendText(".");
-	}
-
-	private void status()
-	{
-		content.ac(HTML.h1("Project status"));
-
-		Element status = content.ac(HTML.span());
-
-		status.appendText("The project is in alpha stage.");
-		status.appendText(
-				" Things are still changing quickly and many things are not easy to use and fully functional yet.");
-		status.appendText(
-				" At this point, some expertise in programming is required to get involved with the project.");
-
-		status.appendText(
-				" To get an impression of what we're getting at, have a look at the ");
-		status.ac(HTML.a(getLink(PathHelper.demo()), "Demo page"));
-		status.appendText(".");
-
-		status.appendText(" You can also follow us on ");
-		status.ac(HTML.a("https://twitter.com/openmetromaps", "Twitter"));
-
-		status.appendText(
-				" or have a look at the contributions we submitted to the OpenStreetMap with ");
-		status.ac(HTML.a("https://www.openstreetmap.org/user/OpenMetroMaps",
-				"our OSM account"));
-		status.appendText(". ");
-	}
-
-	private void license()
-	{
-		content.ac(HTML.h1("License"));
-
-		Element license = content.ac(HTML.span());
-
-		license.appendText("This is a free software project.");
-		license.appendText(
-				" All source code is released under the terms of the ");
-		license.appendText("GNU Lesser General Public License (LGPL).");
-
-		license.appendText(
-				" For details, have a look at the terms of both the ");
-		license.ac(
-				HTML.a("https://www.gnu.org/licenses/lgpl-3.0.html", "LGPL"));
-		license.appendText(" and the ");
-		license.ac(HTML.a("https://www.gnu.org/licenses/gpl-3.0.html", "GPL"));
-		license.appendText(".");
-
-		license.appendText(" We're also producing free data");
-		license.appendText(", which is published under the terms of the ");
-
-		license.ac(HTML.a("https://opendatacommons.org/licenses/odbl/1.0/",
-				"Open Database License (ODbL)"));
-		license.appendText(".");
-
-		license.appendText(
-				" To understand what that means, have a look at the ");
-		license.ac(HTML.a("https://opendatacommons.org/licenses/odbl/summary/",
-				"plain language summary"));
-		license.appendText(" of the ODbL.");
-	}
-
-	private void documentation()
-	{
-		content.ac(HTML.h1("Documentation"));
-
-		content.appendText(
-				"If you're interested in viewing maps, making your own maps or customizing existing ones, have a look at the ");
-		content.ac(HTML.a(getLink(PathHelper.userGuide()), "User Guide"));
-		content.appendText(".");
-
-		content.appendText(
-				" If you would like to help improve the software, read the ");
-		content.ac(HTML.a(getLink(PathHelper.devGuide()), "Developer Guide"));
-		content.appendText(".");
-
-		content.appendText(" There's also a list of ");
-		content.ac(HTML.a(getLink(PathHelper.faq()),
-				"Frequently Asked Questions"));
-		content.appendText(".");
-
-		content.appendText(" Otherwise get in touch via the ");
-		content.ac(HTML.a("https://www.freelists.org/list/openmetromaps",
-				"Mailing List"));
-		content.appendText(".");
 	}
 
 	private void funding()
---
+++ project/dynamic/src/main/resources/markdown/index.md
@@ -0,0 +1,42 @@
+# Our mission
+Schematic maps for public transportation networks are essential tools
+for orientation and navigation in cities. Unfortunately, these maps are
+usually proprietary resources that pose strong limits on what you are
+allowed to do with them. The owners of the material often charge a fee
+for displaying the content on websites, apps or printed material and
+they don't allow you to change anything on the map itself. This alone
+prevents creative use of these maps. Furthermore, you only get a PDF
+document – this makes interactive content hard to create. We're here to
+change that by providing open source tools and file formats for
+creating free schematic metro maps. [[Read more|mission]].
+
+# Project status
+The project is in alpha stage. Things are still changing quickly and
+many things are not easy to use and fully functional yet. At this
+point, some expertise in programming is required to get involved with
+the project. To get an impression of what we're getting at, have a look
+at the [[Demo page|demo]]. You can also follow us on
+[Twitter](https://twitter.com/openmetromaps) or have a
+look at the contributions we submitted to the OpenStreetMap with
+[our OSM account](https://www.openstreetmap.org/user/OpenMetroMaps).
+
+# License
+This is a free software project. All source code is released under the
+terms of the GNU Lesser General Public License (LGPL). For details,
+have a look at the terms of both the
+[LGPL](https://www.gnu.org/licenses/lgpl-3.0.html)
+and the
+[GPL](https://www.gnu.org/licenses/gpl-3.0.html).
+We're also producing free data, which is published under the terms of the
+[Open Database License (ODbL)](https://opendatacommons.org/licenses/odbl/1.0/).
+To understand what that means, have a look at the
+[plain language summary](https://opendatacommons.org/licenses/odbl/summary/)
+of the ODbL.
+
+# Documentation
+If you're interested in viewing maps, making your own maps or
+customizing existing ones, have a look at the [[User Guide|user-guide]].
+If you would like to help improve the software, read
+[[the Developer Guide|developer-guide]]. There's also a list of
+[[Frequently Asked Questions|faq]]. Otherwise get in touch via
+the [Mailing List](https://www.freelists.org/list/openmetromaps).
