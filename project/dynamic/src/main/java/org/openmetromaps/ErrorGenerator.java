72f9b86b03ad8352ee2d5e8359e83f9e11ee2643
Sebastian Kürten <sebastian@topobyte.de>
2018-05-15 17:45:20 +0200

    Let ErrorGenerator extend BaseGenerator

--- project/dynamic/src/main/java/org/openmetromaps/ErrorGenerator.java
+++ project/dynamic/src/main/java/org/openmetromaps/ErrorGenerator.java
@@ -2,11 +2,10 @@ package org.openmetromaps;
 
 import java.io.IOException;
 
-import de.topobyte.pagegen.bootstrap.BootstrapGenerator;
 import de.topobyte.pagegen.core.Context;
 import de.topobyte.webpaths.WebPath;
 
-public class ErrorGenerator extends BootstrapGenerator
+public class ErrorGenerator extends BaseGenerator
 {
 
 	public ErrorGenerator(Context context, WebPath path)
@@ -18,11 +17,8 @@ public class ErrorGenerator extends BootstrapGenerator
 	public void generate() throws IOException
 	{
 		super.generate();
-
-		MainMenu menu = new MainMenu(this);
-		getBuilder().getBody().prependChild(menu);
-
-		getBuilder().getBody().appendChild(new Footer(this));
+		menu();
+		footer();
 	}
 
 }
